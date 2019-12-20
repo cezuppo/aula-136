1189e7239f128d9b86e8e19fe39e47d6004f3426
cezuppo <czuppo@gmail.com>
2019-12-19 21:29:34 -0300

    Segundo teste com mensagem mostrando que o bloco finally foi executado (arquivo existe).

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
-		File file = new File("C:\\temp\\in.txt");
+		File file = new File("D:\\Documentos\\in.txt");
 		Scanner sc = null;
 		try {
 			sc = new Scanner(file);
